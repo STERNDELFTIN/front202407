9103c135a1092c702ac283a505b442360b43271d
STERN <resendo13@gmail.com>
2024-07-17 08:37:14 +0900

    7월 16일 자바스크립트 실습
    7월 16일 자바스크립트 실습

--- JavaScript&JQuery/0716/JavaScript/JS/exCal2.js
+++ JavaScript&JQuery/0716/JavaScript/JS/exCal2.js
@@ -1,19 +1,36 @@
-// 전체 페이지를 다 읽고 난 다음에 실행됨
-window.onload = function() {
-// AND조건 &&
-// OR조건 ||
-// not조건 !
-
 // ----- 저장되어 있는 값
 var id = "main";
 var pw = "A0123";
 
-// ----- 사용자가 입력한 값
+// ex1----- 사용자가 입력한 값 input
 var userId = "main";
 var userPw = "A0123";
 
-var result = (id === userId) && (pw === userPw);
+// ex2-----
+// var userProm = prompt("아이디를 입력하세요");
+// var userPromPwd = prompt("패스워드를 입력하세요");
 
-console.log(result);
+// ex3-----
+// var classA = prompt("A과목 점수를 입력하세요");
+// var classB = prompt("B과목 점수를 입력하세요");
 
+// ex4-----
+// var numNot = 3 > 2;
+// var re = !numNot;
+
+// 전체 페이지를 다 읽고 난 다음에 실행됨
+window.onload = function() {
+    // AND조건 &&, OR조건 ||, not조건 !
+    // var result = (id === userId) && (pw === userPw);
+    // console.log(result);
+
+    // ex2-----
+    // result = (id === userProm) && (pw === userPromPwd);
+    // console.log(result);
+
+    // result = (classA > 70) || (classB > 70)
+    // console.log(result);
+
+    // console.log("3 > 2 => ", numNot);
+    // console.log("!(3 > 2) => ", re);
 }
